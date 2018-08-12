b2e44de248cbd80ef279b79cc8e558a333eaccb6
柳景源 <liujingyuan@jinbank.com.cn>
2018-08-12 17:53:32 +0800

    init

---
+++ sso-client1/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mvn -Dmaven.test.skip=true -U clean install
+
+docker build -t 192.168.99.1:8082/longfeizheng/sso-client1:1.0 .
+
+#docker push 192.168.99.1:8082/longfeizheng/sso-client1:1.0
